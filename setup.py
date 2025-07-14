855a355080dc48493095bcf4c51909c4594fd39f
Manuel <Manuel@DESKTOP-HSEMFHO>
2025-07-14 02:32:25 +0200

    add memory and cpu to container

--- setup.py
+++ setup.py
@@ -1,10 +1,5 @@
 from setuptools import setup, find_packages
-import pkg_resources
-
-with open("requirements.txt", "r") as f:
-    requirements = [str(req) for req in pkg_resources.parse_requirements(f)]
 
 setup(name="langchain-app",
       version="0.0.1",
-      packages=find_packages(),
-      install_requires=requirements)
+      packages=find_packages())
