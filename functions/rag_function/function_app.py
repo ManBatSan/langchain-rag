8d664d629fc8737526c79841da00b5bf72fec830
Manuel <Manuel@DESKTOP-HSEMFHO>
2025-07-14 02:27:52 +0200

    with root

--- functions/rag_function/function_app.py
+++ functions/rag_function/function_app.py
@@ -1,6 +1,6 @@
 import azure.functions as func
 import json
-# from langchain_app.rag_client import answer_question, return_test
+from langchain_app.rag_client import answer_question, return_test
 import logging
 
 
@@ -19,23 +19,22 @@ def call_rag(req: func.HttpRequest) -> func.HttpResponse:
         payload = req.get_json()
         question = payload.get("question")
         return func.HttpResponse(
-            json.dumps({"answer": question, "test": "hhm"}),
-            # json.dumps({"answer": question, "test": return_test()}),
+            json.dumps({"answer": question, "test": return_test()}),
             status_code=200,
             mimetype="application/json"
         )
-        # if not question:
-        #     return func.HttpResponse(
-        #         json.dumps({"error": "Missing 'question'"}),
-        #         status_code=400,
-        #         mimetype="application/json"
-        #     )
-        # answer = answer_question(question)
-        # return func.HttpResponse(
-        #     json.dumps({"answer": answer}),
-        #     status_code=200,
-        #     mimetype="application/json"
-        # )
+        if not question:
+            return func.HttpResponse(
+                json.dumps({"error": "Missing 'question'"}),
+                status_code=400,
+                mimetype="application/json"
+            )
+        answer = answer_question(question)
+        return func.HttpResponse(
+            json.dumps({"answer": answer}),
+            status_code=200,
+            mimetype="application/json"
+        )
     except Exception as e:
         return func.HttpResponse(
             json.dumps({"error": str(e)}),
